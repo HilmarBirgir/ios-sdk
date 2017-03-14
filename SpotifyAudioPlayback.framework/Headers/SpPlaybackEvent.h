6b644d42fcea07446a51294058348818be85e1b3
Hilmar Birgir Ólafsson <hilmarbirgir@gmail.com>
2017-03-14 10:30:14 +0000

    Fixed warnings

--- SpotifyAudioPlayback.framework/Headers/SpPlaybackEvent.h
+++ SpotifyAudioPlayback.framework/Headers/SpPlaybackEvent.h
@@ -67,14 +67,14 @@ typedef enum : NSUInteger {
 	 * \see SpPlaybackSkipToNext
 	 * \deprecated Use SPPlaybackNotifyMetadataChanged instead.
 	 */
-	SPPlaybackNotifyNext,
+	SPPlaybackNotifyNext __attribute__((deprecated)),
 
 	/**
 	 * \brief Playback as skipped to the previous track
 	 * \see SpPlaybackSkipToPrev
 	 * \deprecated Use SPPlaybackNotifyMetadataChanged instead.
 	 */
-	SPPlaybackNotifyPrev,
+	SPPlaybackNotifyPrev __attribute__((deprecated)),
 
 	/**
 	 * \brief "Shuffle" was switched on
@@ -181,7 +181,7 @@ typedef enum : NSUInteger {
 	 *
 	 * \deprecated Use SPPlaybackNotifyMetadataChanged instead.
 	 */
-	SPPlaybackNotifyContextChanged,
+	SPPlaybackNotifyContextChanged __attribute__((deprecated)),
 
 	/**
 	 * \brief Application accepted all samples from the current track
